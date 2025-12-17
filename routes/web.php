10a1b64637548015088f54da4d09a960305df935
igoo07 <helmaesa.putra21@student.uisi.ac.id>
2025-12-18 05:05:56 +0700

    Fix routing and public path binding

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\PortfolioController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome'); // atau view dashboard/portofolio kamu
+});
+
 Route::get('/home', [PortfolioController::class, 'index'])->name('home');
 
 // Halaman-halaman lainnya
@@ -10,6 +14,7 @@ Route::get('/about', [PortfolioController::class, 'about'])->name('about');
 Route::get('/experience', [PortfolioController::class, 'experience'])->name('experience');
 Route::get('/projects', [PortfolioController::class, 'projects'])->name('projects.index');
 Route::get('/contact', [PortfolioController::class, 'contact'])->name('contact');
+
 
 // Route bawaan Breeze (untuk sistem login/admin nanti)
 require __DIR__.'/auth.php';
