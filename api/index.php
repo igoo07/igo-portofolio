369e18f2f64a9a697e4ff26f9d89828da374dd4d
igoo07 <helmaesa.putra21@student.uisi.ac.id>
2025-12-18 05:27:33 +0700

    Include build assets and debug mode

--- api/index.php
+++ api/index.php
@@ -1,3 +1,13 @@
 <?php
-// Jembatan untuk menjalankan Laravel di Vercel
-require __DIR__ . '/../public/index.php';
+// Tampilkan semua error ke layar
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Exception $e) {
+    echo "<h1>Laravel Error:</h1>";
+    echo "<p>" . $e->getMessage() . "</p>";
+    echo "<pre>" . $e->getTraceAsString() . "</pre>";
+}
